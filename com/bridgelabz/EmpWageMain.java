eb9c4eab016641eb32ddc34ff59ffe56fe00ccdf
Akash Das <akashd595@gmail.com>
2022-05-29 14:45:20 +0530

    ADDED: EmpWage

---
+++ com/bridgelabz/EmpWageMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class EmpWageMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+    }
+}
